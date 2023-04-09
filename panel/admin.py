431cf1a62f5910c9e3acf4a2f38690d4b4152794
lfgonzales <lfgonzales@pucp.edu.pe>
2023-04-09 15:11:33 -0500

    first commit

---
+++ panel/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import PersonalRegistrado
+from .models import LiveData
+from .models import Marcacion
+from .models import NoRegistrados
+
+
+# Register your models here.
+
+class PersonalRegistradoAdmin(admin.ModelAdmin):
+    readonly_fields = ("f_registro", )
+
+
+admin.site.register(PersonalRegistrado, PersonalRegistradoAdmin)
+admin.site.register(LiveData)
+admin.site.register(Marcacion)
+admin.site.register(NoRegistrados)
